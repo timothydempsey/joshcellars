156610807fe8c8642dd796b9b79fc2ef0fd109a0
Timothy Dempsey <tim@dempseystudio.com>
2017-10-09 10:53:25 -0400

    Initial Commit

---
+++ about.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description"  content="About Josh Cellars Origin: California Appellation:California, Central Coast, North Coast, Paso Robles Year Founded: 2007 Winemakers :Wayne Donaldson With The Deutsch Family Since 2011 Big, bold and unforgettable. Long Live Josh">
+<meta name="keywords" content="About Wine California red white sauvignon blanc pinot noir merlot legacy chardonnay cabernet sauvignon">
+
+<meta property=”og:title” content="Josh Cellars: About" />
+<meta property=”og:type” content=”article” />
+<meta property=”og:url” content="http://joshcellars.com/about" />
+<meta property=”og:description” content="In 2005, I was in the wine cellar with a friend, someone who happened to be a 3rd generation Sonoma grape grower. We were tinkering around with a new wine that we sourced from a rugged volcanic vineyard in the Red Hills of Lake County California. We were reminiscing about our fathers, and it reminded me of how my Dad used to tinker around the garage, rebuilding old stock cars with his friends. " />
+
+<title>Wine | Josh Cellars | About</title>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/custom2.css"  type="text/css">
+<link rel="stylesheet" href="css/fonts/font-awesome-4.5.0/css/font-awesome.css">
+<!-- custom scrollbar stylesheet -->
+<link rel="stylesheet" href="js/malihu-custom-scrollbar-plugin-3.1.3/jquery.mCustomScrollbar.css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- Custom CSS -->
+<link href="css/round-about.css" rel="stylesheet">
+
+<!-- Age Verification CSS -->
+<link href="css/age-verification.css" rel="stylesheet">
+<style>
+html { 
+  background: url(images/backgrounds/about-josh-cellars_mobile.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 100%;
+  overflow-y:hidden;
+}
+
+	p {
+		text-align:left;
+	}	
+	
+/* media smartphone*/
+
+@media (max-width: 640px) {
+
+	.mCustomScrollBox {
+		height:75%;
+	}	
+	
+</style>
+
+
+<link href="http://www.joshcellars.com/about.php" rel="canonical" />
+</head>
+<body>
+	<!--
+	Start of DoubleClick Floodlight Tag: Please do not remove
+	Activity name of this tag: CNT_Josh Cellars_About Page_2015-11-9
+	URL of the webpage where the tag is expected to be placed: http://www.joshcellars.com/about/
+	This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
+	Creation Date: 11/09/2015
+	-->
+		<script type="text/javascript">
+		var axel = Math.random() + "";
+		var a = axel * 10000000000000;
+		document.write('<iframe src="https://4449509.fls.doubleclick.net/activityi;src=4449509;type=holid0;cat=cnt_j0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+		</script>
+		<noscript>
+		<iframe src="https://4449509.fls.doubleclick.net/activityi;src=4449509;type=holid0;cat=cnt_j0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+		</noscript>
+	<!-- End of DoubleClick Floodlight Tag: Please do not remove -->
+
+<?php include("includes/navig.php"); ?>
+
+<div class="row">
+ 
+   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+   
+   <div id="content-5"><h1 class="lightR">ABOUT JOSH</h1><img src="images/Josh-Cellars-Joseph-Carr-Vintage-Photos.png" class="img-responsive" alt="Joe Carr Josh Vintage Photo" style="max-width:33%;float:right"><p >“In 2005, I was in the wine cellar with a friend, someone who happened to be a 3rd generation Sonoma grape grower. We were tinkering around with a new wine that we sourced from a rugged volcanic vineyard in the Red Hills of Lake County California. We were reminiscing about our fathers, and it reminded me of how my Dad used to tinker around the garage, rebuilding old stock cars with his friends.
+<br/><br/>That rich, approachable wine that we tried back then defines the style and quality that we try to replicate each vintage at Josh Cellars, wines that are bold and expressive, but unassuming and approachable. Funny how it reminds me of someone. My Dad’s name was Joseph, as is mine, but his friends just called him Josh.” – Joseph Carr, Founder
+ <br/><br/>
+“My dad was a soldier in the US Army and a volunteer firefighter in our hometown, things that influence my life today and the charities we work with every year.” <a href="community" style="color:#e4d0b0;font-weight:bold;text-decoration:underline;">Click here</a> to learn more.</p>
+<h1 class="lightR">OUR FOUNDER</h1>
+ <p style="margin-bottom:3.0em">After a decade serving as a world-class sommelier followed by another decade as a wine industry executive, Joseph Carr set out on his own in 2005 to form his family-owned wine company. It was, as Joseph Carr says, “a chance to follow a dream.”
+   <img src="images/Josh-Cellars-Founder-Joseph-Carr.jpg" class="img-responsive" alt="Joe Carr Josh Vintage Photo" style="max-width:33%;float:left;padding:0.5em"> <br/><br/>Influenced by the wines of Bordeaux and Burgundy, Joseph Carr began making wines under a label bearing his name with a winemaking philosophy emphasizing balance, sophistication and approachability. Old world in style, but expressive of California’s best winegrowing regions, the Joseph Carr line of Cabernet Sauvignon and Merlot is sourced from select vineyards in Rutherford, Oakville and the Stags Leap District, and the Chardonnay from the Sonoma Coast.
+    <br/><br/>In 2007, Joseph launched Josh Cellars, wines that represent the best of California: bold, rich wines that are approachable and balanced. Sourced from across many great California winegrowing regions, every vintage represents a labor of love, a commitment to quality, and a very personal promise to make great wine, in honor of his dad, Josh.
+    <br/><br/>Joseph lives with his wife Dee, dog Molly and frequently gets visits from his daughter Cailen at their home in Cape Cod, Massachusetts. He splits his time between the Cape and Napa, “a rough gig,” he admits with a smirk.
+    <br/><br/>
+    <a href="http://www.josephcarrwine.com/" style="color:#FFF;text-decoration:underline;font-weight:bold">Click here</a> to learn more about the Joseph Carr wines.
+    </p>
+ 
+ </div></div>
+  
+
+
+
+</div>
+
+
+
+
+<?php include("includes/footer.php"); ?>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.3.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71534616-2', 'auto');
+  ga('require', 'autotrack');
+  ga('send', 'pageview');
+
+</script>
+
+
+<script src="js/malihu-custom-scrollbar-plugin-3.1.3/jquery.mCustomScrollbar.concat.min.js"></script>
+<script>
+    (function($){
+        $(window).load(function(){
+            $("#content-5").mCustomScrollbar({
+					scrollButtons:{enable:true},
+					theme:"light-thick",
+					scrollbarPosition:"outside"
+				});
+        });
+    })(jQuery);
+</script>
+
+<script src="https://cdn.jsdelivr.net/jquery.cookie/1.4.1/jquery.cookie.min.js"></script>
+
+    
+
+</body>
+</html>
