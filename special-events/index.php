367ca11f3f694912f9448014f5d84797c254b6df
Timothy Dempsey <tim@dempseystudio.com>
2018-03-22 09:53:30 -0400

    Added Special Events

---
+++ special-events/index.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+  <meta name="description"  content="Recipes and Pairings Josh Cellars:  Long Live Josh">
+  <meta name="keywords" content="Special Events Wine California red white sauvignon blanc pinot noir merlot legacy chardonnay cabernet sauvignon">
+  <title>Josh Cellars Wine | Special Events</title>
+  
+ <!-- Bootstrap -->
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/custom2.css"  type="text/css">
+	<link rel="stylesheet" href="../css/fonts/font-awesome-4.5.0/css/font-awesome.css">
+ <!--<link rel="stylesheet" href="../js/Swiper-3.2.7/dist/css/swiper.min.css">-->
+<!-- Custom CSS -->
+<link href="../css/round-about.css" rel="stylesheet">	<!-- why, again? -->
+
+<!-- Age Verification CSS 
+<link href="css/age-verification.css" rel="stylesheet">-->
+<style> 
+	
+	html {
+	 	background-color: #f2f2f2;
+	}
+	
+	.linkColor {
+		color:#184922;
+		font-size:1.33em;
+	}
+	
+	hr {
+		border-color:#B1B1B1;
+	}
+	
+	span {
+		margin:0 auto;
+		text-align:center;
+	}
+	
+	img.thumbnail {
+	margin:0.5em auto;
+	width:235px;
+	height:175px;
+	}
+	
+	iframe {
+	margin-top:2.5em;
+	}
+
+
+</style> 
+
+<!-- Add jQuery library -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+
+
+</head>
+<body>
+	<?php include("../includes/navig.php"); ?>
+	
+  
+  	<section>
+	<div>
+		
+      <div class="rowVideo" >
+        	<div class="col-md-12 col-sm-12 col-xs-12" style="margin-bottom:2.0em;">
+				<h1 class="recipes">SPECIAL EVENTS</h1>
+				<!--<p style="color:#000;width:66%;margin:0 auto;text-align:center"> Watch Joseph Carr, founder of Josh Cellars, speak from the heart on his experiences creating Josh Cellars and on how his love for his father, and the values his father passed down to him, led to the creation of the fantastic wines that Josh Cellars produces today.</p>-->
+				<p style="color:#000;width:66%;margin:0 auto;text-align:center">
+				Josh Cellars is offering special events in the cities below. <br>
+				Click on Participating Locations below to find a participating restaurant near you.</p>
+  	  </div>
+   	  </div>
+       
+	  <div class="rowVideo" >
+         
+         	<div class="col-lg-12 col-md-12 col-sm-12"  style="padding-bottom: 3.0em;"><img src="images/MNH-RWSp18-logoDATES-KO-2.jpg" class="thumbnail pull-center" alt="Chicago Restaurant Week">
+         	<div style="margin:0 auto;text-align:center">
+        			<a href="new-haven-restaurant-week.php" class="linkColor">
+      					<i class="fa fa-map-marker" aria-hidden="true" ></i>Participating Locations
+      				</a><br>
+       				<a href="https://www.infonewhaven.com/dining/nhrwinfo/" target="_blank" class="linkColor">
+        				<i class="fa fa-internet-explorer" aria-hidden="true" ></i>New Haven Restaurant Week Website
+        			</a>
+					
+      			</div></div>
+       		
+         
+			
+			
+       			
+       		
+       			
+        </div>
+       
+        <div class="rowVideo" style="margin:0 auto;text-align:center;height=100vh;margin-bottom:4.0em;">
+         
+         	<div class="col-lg-3 col-md-3 col-sm-12"><img src="images/dinela_lockup_logo_winter_2018.png" class="thumbnail pull-center" alt="Chicago Restaurant Week" style="opacity:0.5;">
+         	<div style="margin:0 auto;text-align:center">
+        			<a href="losangeles-restaurant-week.php" class="linkColor">
+      					<i class="fa fa-map-marker" aria-hidden="true" ></i>Participating Locations
+      				</a><br>
+       				<a href="https://www.discoverlosangeles.com/dinela-los-angeles-restaurant-week/" target="_blank" class="linkColor">
+        				<i class="fa fa-internet-explorer" aria-hidden="true" ></i>Dine L.A. Week Website
+        			</a>
+					
+      			</div></div>
+       		
+         
+			<div class="col-lg-3 col-md-3 col-sm-12"><img src="images/eatitupchicago.jpg" class="thumbnail pull-center" alt="Chicago Restaurant Week" style="opacity:0.5;">
+        		<div style="margin:0 auto;text-align:center">
+        			<a href="chicago-restaurant-week.php" class="linkColor">
+      					<i class="fa fa-map-marker" aria-hidden="true" ></i>Participating Locations
+					</a><br>
+       				<a href="http://www.eatitupchicago.com/" target="_blank" class="linkColor">
+        				<i class="fa fa-internet-explorer" aria-hidden="true" ></i>Chicago Restaurant Week Website
+        			</a>
+					
+      			</div>
+			</div>
+        	
+			
+       			<div class="col-lg-3 col-md-3 col-sm-12" ><img src="images/first-bites-bash.jpg" class="thumbnail pull-center" alt="First Bites Bash" style="opacity:0.5;">
+       			<div style="margin:0 auto;text-align:center">
+        			<a href="#" class="linkColor"><i class="fa fa-map-marker" aria-hidden="true"  ></i>Participating Locations</a><em> Coming soon!</em><br>
+        			<a href="http://www.firstbitesbash.com/" target="_blank" class="linkColor"><i class="fa fa-internet-explorer" aria-hidden="true" ></i>Chicago Restaurant Week Website</a>
+				</div>
+				</div>
+       		</a>
+       		
+        	<div class="col-lg-3 col-md-3 col-sm-12" style="margin-bottom:6em;">
+        	<img src="images/stpaul-minn-restaurant-week.png" class="thumbnail pull-center" alt="Mpls St. Paul" style="opacity:0.5;">
+       			<div style="margin:0 auto;text-align:center">
+        			<a href="minneapolis-st-paul-restaurant-week.php" class="linkColor"><i class="fa fa-map-marker" aria-hidden="true"  ></i>Participating Locations</a><br>
+        			<a href="http://mspmag.com/promotions/restaurantweek" target="_blank" class="linkColor"><i class="fa fa-internet-explorer" aria-hidden="true" ></i>Mpls St. Paul Restaurant Week Website</a>
+			  </div>
+		  </div>
+       		</a>
+       			
+        </div>
+         <div style="clear:both"></div>
+         
+         <!--<div class="rowVideo" style="margin:0 auto;text-align:center;height=100vh;padding-bottom:3.0em;">
+         
+         	<div class="col-lg-2 col-md-2 col-sm-1"></div>
+       		
+         
+			<div class="col-lg-8 col-md-8 col-sm-10">
+       		<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1k9kNwbHZgaCjPyEG513bC4dBDCpROSSQ" width="100%" height="480"></iframe>
+        		<div style="margin:0 auto;text-align:center">
+        			<a href="chicago-restaurant-week.php" class="linkColor">
+      					<i class="fa fa-map-marker" aria-hidden="true" ></i>Participating Locations
+      				</a><br>
+       				<a href="http://www.eatitupchicago.com/" target="_blank" class="linkColor">
+        				<i class="fa fa-internet-explorer" aria-hidden="true" ></i>Chicago Restaurant Week Website
+        			</a>
+					
+      			</div>
+			</div>
+			
+			
+       		
+        	<div class="col-lg-2 col-md-2 col-sm-1"></div>
+       		
+       			
+        </div>-->
+        
+        
+		
+    </div>
+	</section>
+	
+	
+	<div style="clear:both"></div>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71534616-2', 'auto');
+  ga('send', 'pageview');
+
+</script><?php include("../includes/footer.php"); ?>
+ 	
+<script src="../js/src/js/utils.js"></script>
+<script src="../js/src/js/tether.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="../bootstrap-4.0.0-alpha.5-dist/js/bootstrap.js"></script>
+
+</body>
+</html>
